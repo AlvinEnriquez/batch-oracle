05a0f19467f8e0eaee856b2cfe72158c9c08ffe2
AlvinEnriquez <alvinpaoloenriquez@gmail.com>
2024-09-22 21:31:16 +0800

    test commit 1

--- src/main/java/com/alvin/batch_oracle/config/StudentProcessor.java
+++ src/main/java/com/alvin/batch_oracle/config/StudentProcessor.java
@@ -9,6 +9,7 @@ public class StudentProcessor implements ItemProcessor<Student, Student> {
 	@Override
 	public Student process (Student student) throws Exception {
 		// all the business logic goes here
+		// test change1
 		student.setId(null);
 		return student;
 	}
